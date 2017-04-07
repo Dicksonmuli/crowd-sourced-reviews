8e79c7496c292d1cbe633c14c1b460aae489d465
dicksonmuli <dicksonmully6@gmail.com>
2017-04-07 13:13:34 +0300

    review tile- displays all the reviews

--- app/components/review-tile.js
+++ app/components/review-tile.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	//computed heading
+	heading: Ember.computed('review.author', 'review.rating', function(){
+		return this.get('review.author')+', rating:-'+ this.get('review.rating');
+	}),
+	actions: {
+    delete(review) {
+      if (confirm('Are you sure you want to delete this review?')) {
+        this.sendAction('destroyReview', review);
+      }
+    }
+  }
 });
